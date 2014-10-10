34ba64c0a2e786f02b94cb9e54dd13f5217606e2
Alessandro <ale.settimi@gmail.com>
2014-10-10 12:56:14 +0200

    trajectory_generator update

--- src/trajectory_generator.cpp
+++ src/trajectory_generator.cpp
@@ -13,6 +13,7 @@
  * limitations under the License.*/
 
 #include "trajectory_generator.h"
+#include <boost/concept_check.hpp>
 
 trajectory_generator::trajectory_generator(std::string module_prefix, yarp::os::ResourceFinder rf,std::shared_ptr<paramHelp::ParamHelperServer> ph)
 : generic_thread(module_prefix,rf,ph),command_input_interface(module_prefix),command_output_interface(module_prefix+"_out"),status_interface(module_prefix)
@@ -84,6 +85,100 @@ void trajectory_generator::compute_line_trj(trj_generator_input_msg& in, trj_gen
 }
 
 
+void trajectory_generator::compute_circle_trj(trj_generator_input_msg& in, trj_generator_output_msg& out)
+{
+    out.trj.clear();
+  
+    KDL::Frame temp_frame;
+    KDL::Vector temp_vector_p, start_vector_p, dis_vector_p;
+    KDL::Vector temp_vector_r, start_vector_r, dis_vector_r;
+    
+    double circle_angle;
+    double Xf1 = in.center_angle;
+    double radius = in.radius;
+
+    start_vector_p.data[0] = in.start.p.x();
+    start_vector_p.data[1] = in.start.p.y();
+    start_vector_p.data[2] = in.start.p.z();
+    double ro,pi,ya;
+    in.start.M.GetRPY(ro,pi,ya);
+    start_vector_r.data[0] = ro;
+    start_vector_r.data[1] = pi;
+    start_vector_r.data[2] = ya;
+    
+    std::cout<<"TRJ: from ("<<in.start.p.x()<<' '<<in.start.p.y()<<' '<<in.start.p.z()<<' '<<ro<<' '<<pi<<' '<<ya<<") ";
+    
+    dis_vector_p.data[0] = in.displacement.p.x();
+    dis_vector_p.data[1] = in.displacement.p.y();
+    dis_vector_p.data[2] = in.displacement.p.z();
+    double ro_,pi_,ya_;
+    in.displacement.M.GetRPY(ro_,pi_,ya_);
+    dis_vector_r.data[0] = ro_;
+    dis_vector_r.data[1] = pi_;
+    dis_vector_r.data[2] = ya_;
+
+    std::cout<<"to ("<<in.displacement.p.x()<<' '<<in.displacement.p.y()<<' '<<in.displacement.p.z()<<' '<<ro_<<' '<<pi_<<' '<<ya_<<") in "<<in.time<<" [s], R: "<<in.radius<<
+               " [m], angle: "<<in.center_angle<<" [rad], left: "<<in.left<<std::endl;
+    
+//     std::cout<<"(circle ";
+	       
+    double mean_x = (2*start_vector_p.data[0]+dis_vector_p.data[0])/2.0;
+    double mean_y = (2*start_vector_p.data[1]+dis_vector_p.data[1])/2.0;
+    double mean_z = (2*start_vector_p.data[2]+dis_vector_p.data[2])/2.0;
+
+    int index_1 = (2*(in.hand)+(!in.hand));
+    
+    int index_2 = (2*(!in.hand)+in.hand);
+    
+    for(double t=0;t<=in.time;t=t+0.01)
+    {
+        //CircleAngle = 20.0*Xf1/(2.0*Tf*Tf*Tf)*t*t*t                   + (-30.0*Xf1)/(2.0*Tf*Tf*Tf*Tf)*t*t*t*t              + (12.0*Xf1)/(2.0*Tf*Tf*Tf*Tf*Tf)*t*t*t*t*t;
+	circle_angle =  20.0*Xf1/(2.0*pow(in.time,3))*pow(t,3) + (-30.0*Xf1)/(2.0*pow(in.time,4))*pow(t,4) + (12.0*Xf1)/(2.0*pow(in.time,5))*pow(t,5);
+	circle_angle = circle_angle - atan2(dis_vector_p.data[index_2],dis_vector_p.data[0]);
+
+	temp_vector_p.data[index_1] = start_vector_p.data[index_1] + 20.0*(dis_vector_p.data[index_1])/(2.0*pow(in.time,3))*pow(t,3) + (-30.0*dis_vector_p.data[index_1])/(2.0*pow(in.time,4))*pow(t,4) + (12.0*dis_vector_p.data[index_1])/(2.0*pow(in.time,5))*pow(t,5);
+	
+	temp_vector_r = start_vector_r + 20.0*dis_vector_r/(2.0*pow(in.time,3))*pow(t,3) + (-30.0*dis_vector_r)/(2.0*pow(in.time,4))*pow(t,4) + (12.0*dis_vector_r)/(2.0*pow(in.time,5))*pow(t,5);
+	
+        if(!in.left) //RIGHT
+	{
+	    //Xd_v(0) = -Radius*cos(CircleAngle) ;
+	    //Xd_v(1) = Radius*(sin(CircleAngle));
+	    
+	    temp_vector_p.data[0]=-radius*cos(circle_angle)+mean_x;
+	    temp_vector_p.data[index_2]=radius*sin(circle_angle)+in.hand*mean_y+(!in.hand)*mean_z;
+        }
+        else //LEFT
+	{
+	    //Xd_v(0) = -Radius*cos(CircleAngle) ;
+	    //Xd_v(1) = -Radius*(sin(CircleAngle));
+
+	    temp_vector_p.data[0]=-radius*cos(circle_angle)+mean_x;
+	    temp_vector_p.data[index_2]=radius*sin(circle_angle)+in.hand*mean_y+(!in.hand)*mean_z;
+        }
+        
+        //Xd_v(2) = 0.0;
+//         temp_vector_p.data[2] = 0.0 + mean_z;
+
+        //Xd.rows(0,2) = Xinit.rows(0,2) + ROTe*Xd_v.rows(0,2);	
+	
+	temp_frame.p.x(temp_vector_p.data[0]);
+	temp_frame.p.y(temp_vector_p.data[1]);
+	temp_frame.p.z(temp_vector_p.data[2]);
+	temp_frame.M = KDL::Rotation::RPY(temp_vector_r.data[0],temp_vector_r.data[1],temp_vector_r.data[2]);
+
+	out.trj[t] = temp_frame;
+	//std::cout<<t<<' '<<temp_vector_p.data[0]<<' '<<temp_vector_p.data[1]<<' '<<temp_vector_p.data[2]<<' '<<temp_vector_r.data[0]<<' '<<temp_vector_r.data[1]<<' '<<temp_vector_r.data[2]<<' ';
+
+
+    }
+       
+    out.command="result_trj";
+    
+    //std::cout<<')'<<std::endl;
+}
+
+
 void trajectory_generator::run()
 {
     int seq_num;
@@ -101,7 +196,8 @@ void trajectory_generator::run()
 	}
 	if(command=="circle")
 	{
-	    std::cout<<"TODO"<<std::endl;
+	    compute_circle_trj(in_msg,out_msg);
+	    command_output_interface.sendCommand(out_msg,seq_num);   
 	}
     }
 }
