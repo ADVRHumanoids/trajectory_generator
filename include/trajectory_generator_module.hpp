3278b20a83a559cfae6ce8acc688e4cac99109b5
Alessandro <ale.settimi@gmail.com>
2014-10-28 19:14:03 +0100

    Copyright updated

--- include/trajectory_generator_module.hpp
+++ include/trajectory_generator_module.hpp
@@ -1,4 +1,4 @@
-/* Copyright [2014] [Alessandro Settimi, Mirko Ferrati, Arash Ajoudani, Jinoh Lee]
+/* Copyright [2014] [Corrado Pavan, Alessandro Settimi, Mirko Ferrati, Arash Ajoudani, Jinoh Lee]
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
